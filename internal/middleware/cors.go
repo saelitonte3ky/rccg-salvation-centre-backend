2e0cdfce8bb0d0e664848fe691c55cfd38a994cb
Miracle <olapademiracleo@gmail.com>
2025-12-22 22:06:32 +0100

    cor

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -4,7 +4,23 @@ import "github.com/gin-gonic/gin"
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		// Get the origin from the request
+		origin := c.Request.Header.Get("Origin")
+
+		// List of allowed origins
+		allowedOrigins := map[string]bool{
+			"http://localhost:3000":                    true,
+			"https://rccg-salvation-centre.vercel.app": true,
+			"https://www.rccgsalvationcentre.org/":     true,
+		}
+
+		// Allow the request origin
+		if allowedOrigins[origin] {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		} else {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		}
+
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH, OPTIONS")
